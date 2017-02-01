212bccbb4b3a22c98a5911675a7e5b736e39df20
lunding <lunding@me.com>
2017-02-01 16:13:27 +0100

    Added bfs and dfs template.

---
+++ src/DFS_bs.hpp
@@ -0,0 +1,32 @@
+
+
+namespace dfs {
+
+    /**
+     * Construct an array in DFS-order
+     *
+     * @param arr
+     * @param size
+     * @return
+     */
+    int* build(int* arr, int size) {
+        return arr;
+    }
+
+
+    /**
+     * Find the predicate of x (denoted key), which is: max {y \in arr | y ≤ x}.
+     *
+     * @param arr
+     * @param size
+     * @param key
+     * @return
+     */
+    int pred(int* arr, int size, int key) {
+        return key;
+    }
+
+}
+
+
+
