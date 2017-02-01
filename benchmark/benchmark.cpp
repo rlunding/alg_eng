9b1331e7784d84914b2cff3a1c801bfbd2f6186b
lunding <lunding@me.com>
2017-02-01 13:50:21 +0100

    Added format to test.

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+
+struct Algorithm {
+    std::string algorithm;
+    int (*pred)(int*, int, int); // Array pointer, array size, key --> return pred(x)
+    int* (*build)(int*, int); // Array pointer, array size --> return structured array
+};
+
 int main() {
     std::cout << "Let's benchmark!" << std::endl;
     return 0;
